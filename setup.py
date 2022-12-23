1be9403f4e03cd3b140aacd892e1fcd310647ed2
Chris Lam <chrislam@Chriss-MacBook-Air-3.local>
2022-12-23 12:26:39 -0500

    first try at packaging on test PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name = "gaspery",
-    version = "0.1", 
-    packages = find_packages()
+    version = "0.1.1", 
+    packages = find_packages(),
+    url = 'https://github.com/cl3425/gaspery',
+    author = 'Chris Lam',
+    author_email = 'c.lam@ufl.edu',
+    license = 'MIT',
+    install_requires = [
+        'celerite2',
+        'numpy'
+    ]
 )
