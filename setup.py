df1d3dd1b7b09ef4a7fbb34cb01f6383e4cfbd8f
Chris Lam <chrislam@Chriss-MacBook-Air-3.local>
2023-04-04 00:32:45 -0700

    commit for 1.0.2 release on PyPI; also updated companions tutorial to reflect possible path forward on train from SBA to Irvine

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "gaspery",
-    version = "0.1.1", 
+    version = "0.2.0", 
     #packages = find_packages(),
     packages = ['gaspery'],
     package_dir={"":"src"},
