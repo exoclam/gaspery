9dcc992d46e5246fac780ddddf67ce23e334e39b
Chris <chris@Chriss-MacBook-Air-2.local>
2023-05-29 20:38:29 -0400

    uploaded fixed source code for on_vs_off() function to PyPI; updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "gaspery",
-    version = "0.2.0", 
+    version = "0.2.1", 
     #packages = find_packages(),
     packages = ['gaspery'],
     package_dir={"":"src"},
