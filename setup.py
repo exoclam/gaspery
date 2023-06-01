501674d1c116c3351fe73079141f1304f7f35754
Chris <chris@Chriss-MacBook-Air-2.local>
2023-06-01 19:24:34 -0400

    updated version to 0.2.2 for corrected on_vs_off(); also prepping handoff back to dead laptop to run jax/optax for posteriors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "gaspery",
-    version = "0.2.1", 
+    version = "0.2.2", 
     #packages = find_packages(),
     packages = ['gaspery'],
     package_dir={"":"src"},
