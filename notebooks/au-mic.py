2f7dc377fb70f288a59edc2c83c6f4718892203b
Chris Lam <chrislam@Chriss-MacBook-Air-3.local>
2024-03-29 19:09:11 -0400

    tried three different versions of consolidating Figs 5 and 6

--- notebooks/au-mic.py
+++ notebooks/au-mic.py
@@ -67,65 +67,77 @@ cadences2 = np.linspace(3.4976378, 5.04488189, int(dim/2))[1:]
 cadences3 = np.linspace(5.04488189, 6.85, int(dim/2))[1:]
 cadences = np.concatenate([cadences1, cadences2, cadences3])
 
-sigma_k_test = []
-sigma_k_solve_test = []
-sigma_k_stable_test = []
-fi_k_test = []
+def compute_sensitivity_map_residual():
 
-sigma_ks = np.ones(len(n_observations)*len(cadences)).reshape((len(n_observations),len(cadences)))
-fi_ks = np.ones(len(n_observations)*len(cadences)).reshape((len(n_observations),len(cadences)))
+    """
+    Wrapper for computing P_orb vs cadence sensitivity maps for Figure 2 in paper
+    """
 
-for enum_o, o in enumerate(tqdm(n_observations)):
-    for enum_c, c in enumerate(cadences):
-                
-        try:
-            sigma_ks_temp = []
-            for i in range(3):
-                # randomize start date/time, so that we are not subject to accidentally falling on an uninformative phase
-                start_random = random_generator.uniform(start, start+p) 
-                
-                # instantiate Strategy object in order to build time series of observations
-                strategy = strategies.Strategy(n_obs = o, start = start_random, offs=[], dropout=0.)
+    sigma_ks = np.ones(len(n_observations)*len(cadences)).reshape((len(n_observations),len(cadences)))
+    fi_ks = np.ones(len(n_observations)*len(cadences)).reshape((len(n_observations),len(cadences)))
 
-                # build strategy aka time series of observations
-                strat = strategy.gappy(c)
+    for enum_o, o in enumerate(tqdm(n_observations)):
+        for enum_c, c in enumerate(cadences):
+                    
+            try:
+                sigma_ks_temp = []
+                for i in range(10):
+                    # randomize start date/time, so that we are not subject to accidentally falling on an uninformative phase
+                    start_random = random_generator.uniform(start, start+p) 
+                    
+                    # instantiate Strategy object in order to build time series of observations
+                    strategy = strategies.Strategy(n_obs = o, start = start_random, offs=[], dropout=0.)
 
-                ### calculate FI and sigma_K
-                # build covariance matrix, characterized by a correlated noise model of the stellar signal
-                kernel = kernels.ExpSineSquared(scale=Prot, gamma=1/(2*eta**2)) # first term of exponential
-                kernel *= kernels.ExpSquared(scale=Tau) # other term of exponential
-                kernel *= sigma_qp_rv**2 # multiply by scalar
+                    # build strategy aka time series of observations
+                    strat = strategy.gappy(c)
 
-                # instantiate gaspery Star object in order to feed covariance matrix with white/correlated noise
-                star = calculate_fi.Star(sigma_wn_rv = sigma_wn_rv, Tau = Tau, eta = eta, Prot = Prot, sigma_qp_rv = sigma_qp_rv)
-                sigma = star.cov_matrix_general(strat, kernel)
-                sigma += 1e-6 
-                #sigma = inv(sigma) # I want the inverse of the covariance matrix for FIM
+                    ### calculate FI and sigma_K
+                    # build covariance matrix, characterized by a correlated noise model of the stellar signal
+                    kernel = kernels.ExpSineSquared(scale=Prot, gamma=1/(2*eta**2)) # first term of exponential
+                    kernel *= kernels.ExpSquared(scale=Tau) # other term of exponential
+                    kernel *= sigma_qp_rv**2 # multiply by scalar
 
-                # populate arguments for Fisher Info calculator
-                args = np.array(strat), sigma, jnp.array(theta, dtype=float)
+                    # instantiate gaspery Star object in order to feed covariance matrix with white/correlated noise
+                    star = calculate_fi.Star(sigma_wn_rv = sigma_wn_rv, Tau = Tau, eta = eta, Prot = Prot, sigma_qp_rv = sigma_qp_rv)
+                    sigma_correlated = star.cov_matrix_general(strat, kernel)
+                    sigma_white = np.diag(sigma_wn_rv*np.ones(len(strat))) 
+                    sigma_correlated += 1e-6 
+                    sigma_white += 1e-6 
 
-                # calculate FI
-                fim = calculate_fi.clam_jax_fim(*args).block_until_ready()
+                    # populate arguments for Fisher Info calculator
+                    args_correlated = np.array(strat), sigma_correlated, jnp.array(theta, dtype=float)
+                    args_white = np.array(strat), sigma_white, jnp.array(theta, dtype=float)
 
-                # calculate sigma_K
-                inv_fim = inv(fim)
-                sigma_k = np.sqrt(inv_fim)[0][0]
+                    # calculate FI
+                    fim_correlated = calculate_fi.clam_jax_fim(*args_correlated).block_until_ready()
+                    fim_white = calculate_fi.clam_jax_fim(*args_white).block_until_ready()
 
-                # for 2D plots and testing
-                #sigma_ks[enum_o][enum_c] = sigma_k
-                ###sigma_ks_stable[enum1][enum2] = sigma_k_stable
-                ###sigma_ks_solve[enum1][enum2] = sigma_k_solve   
-                sigma_ks_temp.append(sigma_k)
-                
-        except Exception as e:
-            print(e, o, c)
-            sigma_ks[enum_o][enum_c] = np.nan
+                    # calculate sigma_K
+                    inv_fim_correlated = inv(fim_correlated)
+                    sigma_k_correlated = np.sqrt(inv_fim_correlated)[0][0]
 
-        sigma_k_all = np.nanmean(sigma_ks_temp)
-        sigma_ks[enum_o][enum_c] = sigma_k_all
+                    inv_fim_white = inv(fim_white)
+                    sigma_k_white = np.sqrt(inv_fim_white)[0][0]
 
+                    # for 2D plots and testing
+                    #sigma_ks[enum_o][enum_c] = sigma_k
+                    ###sigma_ks_stable[enum1][enum2] = sigma_k_stable
+                    ###sigma_ks_solve[enum1][enum2] = sigma_k_solve   
+                    sigma_ks_temp.append(np.abs(sigma_k_correlated - sigma_k_white))
+                    
+            except Exception as e:
+                print(e, o, c)
+                sigma_ks[enum_o][enum_c] = np.nan
 
+            sigma_k_all = np.nanmean(sigma_ks_temp)
+            sigma_ks[enum_o][enum_c] = sigma_k_all
+
+    return sigma_ks
+
+sigma_ks_residual = compute_sensitivity_map_residual()
+print(sigma_ks_residual)
+print(np.nanmax(sigma_ks_residual), np.nanmin(sigma_ks_residual))
+"""
 x, y = np.meshgrid(cadences, n_observations)
 fig = plt.pcolormesh(y,x,sigma_ks, norm=matplotlib.colors.LogNorm())
 
@@ -135,4 +147,19 @@ plt.xlabel("number of observations")
 plt.ylabel("time between observations [day]")
 plt.title(f"correlated noise") 
 plt.savefig(path + f"plots/n_obs-vs-cadence-correlated-meters-averaged.png", format="png")
+plt.show()
+"""
+
+"""
+I want to see what the difference between white and correlated noise is, for each case on these plots. 
+"""
+x, y = np.meshgrid(cadences, n_observations)
+fig = plt.pcolormesh(y,x,sigma_ks_residual, norm=matplotlib.colors.LogNorm())
+
+cbar = plt.colorbar(fig)
+cbar.ax.set_ylabel(r"$\sigma_k$[m/s]")
+plt.xlabel("number of observations")
+plt.ylabel("time between observations [day]")
+plt.title(f"correlated noise") 
+plt.savefig(path + f"plots/n_obs-vs-cadence-correlated-vs-white-meters-averaged.png", format="png")
 plt.show()
